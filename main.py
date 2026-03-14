e143bd8471c83394087997692486e328a13c085c
chfrchko5 <r4ger7@gmail.com>
2026-03-14 14:16:50 -0400

    locked in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 index_file = "index.html"
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template(index_file)
+
+
+@app.route("/convert-length", methods=["POST"])
+def convert_length():
+    a = request.form["lengthFrom"]
+    b = request.form["lengthTo"]
+    c = request.form["lengthInput"]
+
+    return f"you want {c} to go from {a} to {b} ?"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
