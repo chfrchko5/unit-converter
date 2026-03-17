db9d94140bf30bc073fe1b74df0d84707540923b
chfrchko5 <r4ger7@gmail.com>
2026-03-17 12:17:32 -0400

    logic done

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template(index_file)
 
-
 @app.route("/convert-length", methods=["POST"])
 def convert_length():
     lengthFrom = request.form["lengthFrom"]
